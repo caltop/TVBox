876b4cf159fdfd50656cb56e44ac0a435f8c8c70
calendre <707539386@qq.com>
2022-10-27 11:05:56 +0000

    update libs/drpy.js.
    Signed-off-by: calendre <707539386@qq.com>

--- libs/drpy.js
+++ libs/drpy.js
@@ -33,7 +33,7 @@ function init_test(){
 }
 
 let rule = {};
-const VERSION = '3.9.15beta1';
+const VERSION = '3.9.16';
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -588,8 +588,11 @@ const parseTags = {
                 sp.splice(sp.length - 1);
                 if (sp.length > 1) {
                     for (let i in sp) {
-                        if (!SELECT_REGEX.test(sp[i])) {
-                            sp[i] = sp[i] + ':eq(0)';
+                        //Javascript自定义Array.prototype干扰for-in循环
+                        if(sp.hasOwnProperty(i)){
+                            if (!SELECT_REGEX.test(sp[i])) {
+                                sp[i] = sp[i] + ':eq(0)';
+                            }
                         }
                     }
                 } else {
@@ -639,10 +642,12 @@ const parseTags = {
             if (parse.indexOf('&&') > -1) {
                 let sp = parse.split('&&');
                 for (let i in sp) {
-                    if (!SELECT_REGEX_A.test(sp[i]) && i < sp.length - 1) {
-                        if(sp[i]!=='body'){
-                            // sp[i] = sp[i] + ':eq(0)';
-                            sp[i] = sp[i] + ':first';
+                    if(sp.hasOwnProperty(i)){
+                        if (!SELECT_REGEX_A.test(sp[i]) && i < sp.length - 1) {
+                            if(sp[i]!=='body'){
+                                // sp[i] = sp[i] + ':eq(0)';
+                                sp[i] = sp[i] + ':first';
+                            }
                         }
                     }
                 }
@@ -976,6 +981,7 @@ function checkHtml(html,url,obj){
     if(/\?btwaf=/.test(html)){
         let btwaf = html.match(/btwaf(.*?)"/)[1];
         url = url.split('#')[0]+'?btwaf'+btwaf;
+        print('宝塔验证访问链接:'+url);
         html = request(url,obj);
     }
     return html
@@ -1270,6 +1276,13 @@ function homeVodParse(homeVodObj){
     let t2 = (new Date()).getTime();
     console.log('加载首页推荐耗时:'+(t2-t1)+'毫秒');
     // console.log(JSON.stringify(d));
+    if(rule.图片来源){
+        d.forEach(it=>{
+            if(it.vod_pic&&it.vod_pic.startsWith('http')){
+                it.vod_pic = it.vod_pic + rule.图片来源;
+            }
+        });
+    }
     return JSON.stringify({
         list:d
     })
@@ -1397,6 +1410,13 @@ function categoryParse(cateObj) {
             console.log(e.message);
         }
     }
+    if(rule.图片来源){
+        d.forEach(it=>{
+            if(it.vod_pic&&it.vod_pic.startsWith('http')){
+                it.vod_pic = it.vod_pic + rule.图片来源;
+            }
+        });
+    }
     // print(d);
     return d.length<1?'{}':JSON.stringify({
         'page': parseInt(cateObj.pg),
@@ -1515,6 +1535,14 @@ function searchParse(searchObj) {
             return '{}'
         }
     }
+    if(rule.图片来源){
+        d.forEach(it=>{
+            if(it.vod_pic&&it.vod_pic.startsWith('http')){
+                it.vod_pic = it.vod_pic + rule.图片来源;
+            }
+        });
+    }
+    // print(d);
     return JSON.stringify({
         'page': parseInt(searchObj.pg),
         'pagecount': 10,
@@ -1686,16 +1714,31 @@ function detailParse(detailObj){
         if(p.lists){
             if(p.lists.startsWith('js:')){
                 print('开始执行lists代码:'+p.lists);
-                if(html&&typeof (html)!=='string'){
-                    // 假装是jq的对象拿来转换一下字符串,try为了防止json的情况报错
-                    try {
-                        html = html.rr(html.ele).toString();
-                    }catch (e) {}
+                try {
+                    if(html&&typeof (html)!=='string'){
+                        // 假装是jq的对象拿来转换一下字符串,try为了防止json的情况报错
+                        try {
+                            html = html.rr(html.ele).toString();
+                        }catch (e) {}
+                    }
+                    var input = MY_URL;
+                    var play_url = '';
+                    eval(p.lists.replace('js:',''));
+                    for(let i in LISTS){
+                        if(LISTS.hasOwnProperty(i)){
+                            // print(i);
+                            try {
+                                LISTS[i] = LISTS[i].map(it=>it.split('$').slice(0,2).join('$'));
+                            }catch (e) {
+                                print('格式化LISTS发生错误:'+e.message);
+                            }
+                        }
+                    }
+                    vod_play_url = LISTS.map(it=>it.join('#')).join(vod_play_url);
+                }catch (e) {
+                    print('js执行lists: 发生错误:'+e.message);
                 }
-                var input = MY_URL;
-                var play_url = '';
-                eval(p.lists.replace('js:',''));
-                vod_play_url = LISTS.map(it=>it.join('#')).join(vod_play_url);
+
             }else{
                 let list_text = p.list_text||'body&&Text';
                 let list_url = p.list_url||'a&&href';
@@ -1737,6 +1780,9 @@ function detailParse(detailObj){
         }
         vod.vod_play_url = vod_play_url;
     }
+    if(rule.图片来源 && vod.vod_pic && vod.vod_pic.startsWith('http')){
+        vod.vod_pic = vod.vod_pic + rule.图片来源;
+    }
     if(!vod.vod_id){
         vod.vod_id = vod_id;
     }
@@ -1777,6 +1823,7 @@ function playParse(playObj){
     let common_play = {
         parse:1,
         url:input,
+        // url:urlencode(input),
         jx:tellIsJx(input)
     };
     let lazy_play;
@@ -1889,6 +1936,7 @@ function playParse(playObj){
 
         rule.timeout = rule.timeout||5000;
         rule.encoding = rule.编码||rule.encoding||'utf-8';
+        rule.图片来源 = rule.图片来源||'';
         rule.play_json = rule.hasOwnProperty('play_json')?rule.play_json:[];
         if(rule.headers && typeof(rule.headers) === 'object'){
             try {
