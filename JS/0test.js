b49a712ef48b618e6cb5dbb8d2b8265aa082d35f
calendre <707539386@qq.com>
2022-11-18 00:17:31 +0000

    update JS/0test.js.
    Signed-off-by: calendre <707539386@qq.com>

--- JS/0test.js
+++ JS/0test.js
@@ -1,9 +1,9 @@
 var rule = {
-    title:'KOK影视',
-    host:'https://www.kokyy.com',
+    title:'超前点播',
+    host:'https://bzhanyy.com',
     // homeUrl:'/',
-    url:'/koks/fyclass/page/fypage.html',
-    searchUrl:'/kokso/page/fypage/wd/**.html',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
@@ -11,8 +11,8 @@ var rule = {
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&电视剧&综艺&动漫&纪录片',
-    class_url:'kokdy&kokds&kokzy&kokdm&kokjl',
+    class_name:'电影&电视剧&综艺&动漫&国产剧&港台剧&日韩剧&欧美剧',
+    class_url:'1&2&3&4&13&14&15&16',
     play_parse:true,
     lazy:'',
     limit:6,
@@ -20,5 +20,5 @@ var rule = {
     double:true, // 推荐内容是否双层定位
     一级:'.movie-list-body&&.movie-list-item;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
     二级:{"title":"h1&&Text;.cr3&&Text","img":".poster&&img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.scroll-content&&Text;.title-block&&.starLink&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text","content":".detailsTxt&&Text","tabs":".swiper-wrapper&&a","lists":".play_list_box:eq(#id)&&.content_playlist li"},
-    搜索:'.movie-list-body&&.movie-search-list;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
+    搜索:'.movie-list-body&&.vod-search-list;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
 }
