651656f2120e9754be896a1646ed69b9703cbe7f
calendre <707539386@qq.com>
2023-03-17 04:40:06 +0000

    update lives/tv.js.
    Signed-off-by: calendre <707539386@qq.com>

--- lives/tv.js
+++ lives/tv.js
@@ -3,5 +3,5 @@
 {"name":"多多","url":"https://yydsys.top/duo/txt/v.txt"},
 {"name":"饭太硬","url":"http://ftyyy.tk/live.txt"},
 {"name":"肥猫","url":"http://我不是.肥猫.love:63/TV/tvzb.txt"},
-{"name":"MTV","url":"https://gitee.com/wdc558/TVBox/raw/master/lives/WanShouMTV.m3u"}
+{"name":"MTV","url":"https://gitee.com/wdc558/TVBox/raw/master/lives/WanShouMTV.m3u8"}
 ]
