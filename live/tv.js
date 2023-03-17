a5066c77d7f8c55c289dcd880d90c6bf0f3808b0
calendre <707539386@qq.com>
2023-03-17 03:04:18 +0000

    update test.json.
    Signed-off-by: calendre <707539386@qq.com>

---
+++ live/tv.js
@@ -0,0 +1,7 @@
+[
+{"name": "肥猫",     "url": "http://我不是.肥猫.love:63/TV/tvzb.txt"},
+{"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
+{"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
+{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
+{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt"}
+]
