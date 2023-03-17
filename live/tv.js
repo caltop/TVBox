8b336b9be612f2d2ab0fef9055be83f0a2be3b1f
calendre <707539386@qq.com>
2023-03-17 03:04:57 +0000

    update live/tv.js.
    Signed-off-by: calendre <707539386@qq.com>

--- live/tv.js
+++ live/tv.js
@@ -1,7 +1,7 @@
 [
-{"name": "肥猫",     "url": "http://我不是.肥猫.love:63/TV/tvzb.txt"},
-{"name": "多多",     "url": "https://yydsys.top/duo/txt/v.txt"},
-{"name": "巧计",     "url": "https://ghproxy.com/https://raw.githubusercontent.com/dxawi/1/main/tvlive.txt"},
-{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0"},
-{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt"}
+{"name":"☪夕✿晨","url":"https://gitee.com/wdc558/TVBox/raw/master/live/live.txt"},
+{"name":"多多","url":"https://yydsys.top/duo/txt/v.txt"},
+{"name":"饭太硬","url":"http://ftyyy.tk/live.txt"},
+{"name":"肥猫","url":"http://我不是.肥猫.love:63/TV/tvzb.txt"},
+{"name":"FongMi","url":"https://gh-proxy.com/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/live.json"}
 ]
