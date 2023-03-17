124089c70bad8f4589705bb5c4f02a5e4d63777a
calendre <707539386@qq.com>
2023-03-17 13:54:00 +0000

    update lives/tv.js.
    Signed-off-by: calendre <707539386@qq.com>

--- lives/tv.js
+++ lives/tv.js
@@ -1,7 +1,7 @@
 [
-{"name":"☪夕✿晨","url":"https://gitee.com/wdc558/TVBox/raw/master/lives/live.txt&&&https://gitee.com/wdc558/TVBox/raw/master/lives/pic/yuexi.png"},
-{"name":"多多","url":"https://yydsys.top/duo/txt/v.txt&&&https://api.isoyu.com/mm_images.php"},
-{"name":"饭太硬","url":"http://ftyyy.tk/live.txt&&&https://gitee.com/wdc558/TVBox/raw/master/lives/pic/iptv.png"},
-{"name":"肥猫","url":"http://我不是.肥猫.love:63/TV/tvzb.txt&&&https://gitee.com/wdc558/TVBox/raw/master/lives/pic/iptv.png"},
-{"name":"MTV","url":"https://gitee.com/wdc558/TVBox/raw/master/lives/WanShouMTV.m3u8&&&https://gitee.com/wdc558/TVBox/raw/master/lives/pic/iptv.png"}
+{"name":"☪夕✿晨","url":"https://gitee.com/wdc558/TVBox/raw/master/lives/live.txt&&&https://api.caonm.net/api/mc/index.php"},
+{"name":"多多","url":"https://yydsys.top/duo/txt/v.txt&&&https://api.caonm.net/api/mc/index.php"},
+{"name":"饭太硬","url":"http://ftyyy.tk/live.txt&&&https://api.caonm.net/api/mc/index.php"},
+{"name":"肥猫","url":"http://我不是.肥猫.love:63/TV/tvzb.txt&&&https://api.caonm.net/api/mc/index.php"},
+{"name":"MTV","url":"https://gitee.com/wdc558/TVBox/raw/master/lives/WanShouMTV.m3u8&&&https://api.caonm.net/api/mc/index.php"}
 ]
